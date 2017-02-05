cac6c68e7bfa8c5504e1c8779ee672a9c033b117
ezekielchentnik <zeek@ezekielchentnik.com>
2017-02-05 17:46:23 -0600

    update

--- src/app/entry.js
+++ src/app/entry.js
@@ -2,13 +2,14 @@ import { h, render } from 'preact' // eslint-disable-line no-unused-vars
 import App from './components/App'
 import createStore from './store/createStore'
 import { fetchInitialState, updateLocation } from './store/actions/ActionCreators'
+import { getCurrentUrl } from './store/selectors/meta'
 import ensurePolyfills from './utils/ensurePolyfills'
 import clientFetch from './utils/makeFetch'
 
 const app = document.getElementById('app')
 const store = createStore(window.__STATE__, clientFetch)
 
-ensurePolyfills(() => {
+ensurePolyfills(() => { // assumes no pollys in components
   store.dispatch(fetchInitialState())
 })
 
@@ -17,9 +18,9 @@ window.addEventListener('popstate', (e) => {
 })
 
 store.subscribe(() => {
-  const { location } = store.getState()
-  if(window.location.pathname + window.location.search !== location.url){
-    window.history.pushState({}, '', location.url)
+  const currentUrl = getCurrentUrl(store.getState())
+  if (window.location.pathname + window.location.search !== currentUrl) {
+    window.history.pushState({}, '', currentUrl)
   }
 })
 store.dispatch(updateLocation(window.location.pathname + window.location.search))
