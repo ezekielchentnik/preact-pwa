0d783a2c5478a8b105aeb62a9989d96a5ba156d2
ezekielchentnik <zeek@ezekielchentnik.com>
2017-02-05 20:52:19 -0600

    update

--- src/app/store/selectors/articles.js
+++ src/app/store/selectors/articles.js
@@ -3,7 +3,7 @@
 export const getArticles = (state) => state.articles.collection
 export const getArticle = (state) => { // todo: fix fugg it hack, make bullet proof
   const id = state.meta.currentUrl.split('/articles/')[1]
-  const articles = state.articles.collection.filter((article) => article.id === id)
+  const articles = state.articles.collection.filter((article) => article.id.toString() === id)
   return articles.length ? articles[0] : null
 }
 export const getHasFetchedArticles = (state) => state.articles.hasFetched
