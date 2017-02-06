d4540ebf4aecc39911e63aac0269c317caa1c2df
ezekielchentnik <zeek@ezekielchentnik.com>
2017-02-05 19:02:33 -0600

    update

--- src/server/middleware/cacheControl.js
+++ src/server/middleware/cacheControl.js
@@ -1,5 +1,5 @@
 export default () => (req, res, next) => {
-  // res.header('Cache-Control', 'public, max-age=31536000, no-cache')
-  res.header('Cache-Control', 'no-cache, no-store, must-revalidate')
+  res.header('Cache-Control', 'public, max-age=31536000, no-cache')
+  // res.header('Cache-Control', 'no-cache, no-store, must-revalidate')
   next()
 }
