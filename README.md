8260ce9491de55b9773c792fd8ebc913a63cd5d1
ezekielchentnik <zeek@ezekielchentnik.com>
2017-02-09 08:21:55 -0600

    update

--- README.md
+++ README.md
@@ -9,22 +9,23 @@ Live version: [https://preact-pwa.appspot.com/](https://preact-pwa.appspot.com/)
 ## bundle sizes & perf
 ```
 Bundle Size:            27.55 KB, Gzipped size: 9.79 KB
-First visual change:    226 ms
-Last visual change:     301 ms
-Speed Index:            297.0
-Perceptual Speed Index: 280.8
+Webpage test:           A, A, A
+pagespeed:              100
+lighthouse:             100
 ```
 we can shave off ~10 KB, ~3kb Gzipped by pruning `preact-redux` & manually connecting
 
 ## Features
 
 - Progressive Web App enabled with [service workers](https://developers.google.com/web/fundamentals/getting-started/primers/service-workers)
+- Offline capable with [service workers](https://developers.google.com/web/fundamentals/getting-started/primers/service-workers)
 - Universal JavaScript (isomorphic rendering)
+- Asset Versioning, long term caching, & cache busting for browser that do not support service workers via [node-rev](https://www.npmjs.com/package/node-rev)
 - Modern JavaScript syntax with [ES6](https://github.com/lukehoban/es6features) via [buble](https://buble.surge.sh/guide/).
 - Performant bundles via [rollup](http://rollupjs.org/).
 - Component-based UI architecture via [Preact](https://preactjs.com/).
 - Application state management w/time-travel debugging via [Redux](https://github.com/gaearon/redux).
-- CSS built with [Sass](http://sass-lang.com/), [tachyons](http://tachyons.io/), and optimized with [purify-css](https://github.com/purifycss/purifycss).
+- CSS built with [Sass](http://sass-lang.com/) and optimized with [purify-css](https://github.com/purifycss/purifycss).
 - Async actions handled with [redux-thunk](https://github.com/gaearon/redux-thunk), [isomorphic-fetch](https://github.com/matthew-andrews/isomorphic-fetch), and [promises](https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Promise).
 - Node server is built with [express](http://expressjs.com/).
 - Linting is handled with [Standard](http://standardjs.com/).
--- src/app/components/Splash.js
+++ src/app/components/Splash.js
@@ -3,12 +3,26 @@ import { h } from 'preact' // eslint-disable-line no-unused-vars
 export default () => (
   <div className='Splash page'>
     <div className='card'>
-      <h1>Welcome</h1>
-      <p><em>A `Super fast progressive web app` with a small footprint & minimal dependancies.  </em></p>
+      <h2>Welcome</h2>
+      <p><em>A `Super fast progressive web app` with a small footprint & minimal dependancies.</em></p>
     </div>
     <div className='card'>
-      <h1>Welcome</h1>
-      <p><em>A `Super fast progressive web app` with a small footprint & minimal dependancies.  </em></p>
+      <h2>Features</h2>
+      <ul>
+        <li>Progressive Web App enabled with <a href='https://developers.google.com/web/fundamentals/getting-started/primers/service-workers'>service workers</a></li>
+        <li>Offline capable with <a href='https://developers.google.com/web/fundamentals/getting-started/primers/service-workers'>service workers</a></li>
+        <li>Mobile friendly with manifest.json</li>
+        <li>Universal JavaScript (isomorphic rendering)</li>
+        <li>Asset Versioning, long term caching, and cache busting for browser that do not support service workers via <a href='https://www.npmjs.com/package/node-rev'>node-rev </a></li>
+        <li>Modern JavaScript syntax with <a href='https://github.com/lukehoban/es6features via buble https://buble.surge.sh/guide/'>ES6 </a>.</li>
+        <li>Performant bundles via <a href='http://rollupjs.org/'>rollup</a>.</li>
+        <li>Component-based UI architecture via <a href='https://preactjs.com/'>Preact</a>.</li>
+        <li>Application state management w/time-travel debugging via <a href='https://github.com/gaearon/redux'>Redux</a>.</li>
+        <li>CSS built with <a href='http://sass-lang.com/'>Sass</a> and optimized with <a href='https://github.com/purifycss/purifycss'>purify-css </a>.</li>
+        <li>Async actions handled with <a href='https://github.com/gaearon/redux-thunk'>redux-thunk</a>, <a href='https://github.com/matthew-andrews/isomorphic-fetch'>fetch</a>.</li>
+        <li>Node server is built with <a href='http://expressjs.com/'>express</a>.</li>
+        <li>Linting is handled with <a href='http://standardjs.com/'>Standard</a>.</li>
+      </ul>
     </div>
   </div>
 )
