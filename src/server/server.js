898ef8d6be7a9bedf6fbc698d54792422a3633e9
ezekielchentnik <zeek@ezekielchentnik.com>
2017-02-09 04:30:30 -0600

    update

--- src/server/server.js
+++ src/server/server.js
@@ -2,7 +2,7 @@ require('source-map-support').install()
 import 'isomorphic-fetch'
 import app from './app'
 
-const server = app.listen(process.env.PORT || 5000, () => {
+const server = app.listen(process.env.PORT || 8080, () => {
   console.log(`[server] app on http://localhost:${server.address().port} - ${app.settings.env}`)
 })
 
