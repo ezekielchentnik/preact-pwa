6c58430a7705b27b1187f68ab2bc25662f2766c2
ezekielchentnik <zeek@ezekielchentnik.com>
2017-02-09 08:27:46 -0600

    update

--- src/app/components/About.js
+++ src/app/components/About.js
@@ -4,8 +4,7 @@ export default () => (
   <div className='Splash page'>
     <div className='card'>
       <h2>About</h2>
-      <p>Make it fast</p>
-      <h3>ezekielchentnik/preact-pwa</h3>
+      <strong>ezekielchentnik/preact-pwa</strong>
       <p><a href='https://github.com/ezekielchentnik/preact-pwa'>Super fast progressive web app with small footprint & minimal dependancies</a></p>
     </div>
   </div>
