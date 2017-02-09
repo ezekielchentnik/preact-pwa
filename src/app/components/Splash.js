394213cd805dbedf954cb276f9de5e5664385c65
ezekielchentnik <zeek@ezekielchentnik.com>
2017-02-09 06:44:47 -0600

    update

--- src/app/components/Splash.js
+++ src/app/components/Splash.js
@@ -3,7 +3,15 @@ import { h } from 'preact' // eslint-disable-line no-unused-vars
 export default () => (
   <div className='Splash page'>
     <div className='card'>
-      <p>[splash]</p>
+      <h1>Welcome</h1>
+      <p>This is a super fast progressive web app.</p>
+    </div>
+    <div className='card'>
+      <h2>Features</h2>
+      <ul>
+        <li></li>
+        <li></li>
+      </ul>
     </div>
   </div>
 )
